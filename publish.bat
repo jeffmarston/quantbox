6a97b058b498b6abe24247a183bfedc57518e0b2
Rob Baxter <rbaxter1@gmail.com>
2019-07-24 12:46:48 -0400

    updated readme

--- publish.bat
+++ publish.bat
@@ -2,11 +2,9 @@ mkdir blackboxsim\server
 mkdir blackboxsim\client\dist
 
 cd client
-REM npm run build 
 xcopy dist ..\blackboxsim\client\dist /Y /S
 
 cd ..\server
-dotnet build
 dotnet publish -r win-x64 -c Release --self-contained
 xcopy bin\Release\netcoreapp2.2\win-x64\publish ..\blackboxsim\server /Y /S
 cd ..
@@ -14,6 +12,6 @@ cd ..
 copy install.bat .\blackboxsim\  /Y 
 copy readme.md .\blackboxsim\  /Y
 
-REM "C:\Program Files\7-Zip\7z.exe" a blackboxsim_setup.exe -mmt -mx5 -sfx blackboxsim\
+"C:\Program Files\7-Zip\7z.exe" a blackboxsim_setup.exe -mmt -mx5 -sfx blackboxsim\
 
-REM xcopy blackboxsim_setup.exe c:\share\ /Y /S
+copy blackboxsim_setup.exe c:\share\ /Y
